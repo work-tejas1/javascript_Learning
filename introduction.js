00e07b702f2e47fedd18cd6dd7c290173440beb5
Tejas <51386360+work-tejas1@users.noreply.github.com>
2022-02-19 16:31:27 +0100

    - First commit (day 1 commit)
    - Day 1 (setup, variable, declaration and Day 1: Exercises)

---
+++ introduction.js
@@ -0,0 +1,36 @@
+/*
+console.log('We can also create a string using a backtick')
+//---------------------------------------------------
+let firstName = "MaxMuller"
+console.log(firstName) // undefined, because it is not assigned to a value yet
+
+//---------------------------------------------------
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+//--------------------------------------------------------
+*/
+// Declaring different variables of different data types
+let firstName = 'Asabeneh' // first name of a person
+let lastName = 'Yetayeh' // last name of a person
+let country = 'Finland' // country
+let city = 'Helsinki' // capital city
+let age = 100 // age in years
+let isMarried = true
+
+console.log(firstName, lastName, country, city, age, isMarried)
+
+// Variables can also be declaring in one line separated by comma
+let firstName1 = 'User1', // name of a person
+job = 'SOftware Engineer',
+live = 'Helsinki'
+console.log(firstName1, job, live)
+
+//Assign variable in one line and print on console
+let age1 = 25,
+age2 = 28
+
+console.log(`My age is ${age1} and my friend is ${age2} years old`)
